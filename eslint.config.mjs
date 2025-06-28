c4ec37d1e128c448fddb1598e00e326a2f414451
femartinezgarcia <fernandomartgarc@gmail.com>
2025-06-28 05:17:21 +0200

    build(eslint): add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,34 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import js from '@eslint/js';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-export default [
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig([
+  globalIgnores(['out/**', 'node_modules/**', '.vscode/**']),
   {
     files: ['**/*.ts'],
-  },
-  {
     plugins: {
       '@typescript-eslint': typescriptEslint,
     },
-
     languageOptions: {
       parser: tsParser,
       ecmaVersion: 2022,
       sourceType: 'module',
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
     },
-
-    rules: {
-      '@typescript-eslint/naming-convention': [
-        'warn',
-        {
-          selector: 'import',
-          format: ['camelCase', 'PascalCase'],
-        },
-      ],
-
-      curly: 'warn',
-      eqeqeq: 'warn',
-      'no-throw-literal': 'warn',
-      semi: 'warn',
-    },
+    extends: [
+      js.configs.recommended,
+      '@typescript-eslint/recommended-type-checked',
+      '@typescript-eslint/stylistic-type-checked',
+    ],
   },
-];
+  eslintConfigPrettier,
+]);
